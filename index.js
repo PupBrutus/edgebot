2f1cfb9caad63442fe071e1f3bc4bd922d9dd612
Pup Brutus <PupBrutus@users.noreply.github.com>
2020-06-20 23:01:28 -0700

    removing comment on arguements line

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const Discord = require("discord.js");
 
 const bot = new Discord.Client({disableEveryone: true});
 
-
 bot.on('ready', async ()=>{
     console.log(`${bot.user.username} is online`)
     bot.user.setActivity("Driving you crazy", {type: "STREAMING"});
 })
 
-bot.on('message', (message) =>{
+bot.on('message', async (message) =>{
     if(message.author.bot || message.channel.type === "dm") return;
 
     let prefix = botconfig.prefix;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
-    //let args = messageArray.Slice[0];
+    let args = messageArray.Slice[0];
 
     if(cmd === `${prefix}hello`){
         return message.channel.send("Hello")

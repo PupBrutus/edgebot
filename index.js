fae593e43f890c2b2d551f1f078f0c107feeef93
Pup Brutus <PupBrutus@users.noreply.github.com>
2020-06-20 23:12:50 -0700

    spelling error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const botconfig = require("./botconfig.json");
-const botsecrets = requre("./botsecrets.json")
+const botsecrets = require("./botsecrets.json")
 const colors = require("./botconfig.json");
 const Discord = require("discord.js");
 
